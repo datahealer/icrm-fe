f5d7f5089b7dd800bb9e3328a86cd1fd6d89db59
GargHardik97291 <hgarg@k12.com>
2024-10-09 16:20:30 +0530

    small test change

--- src/views/admin/client/components/AddDrawer.jsx
+++ src/views/admin/client/components/AddDrawer.jsx
@@ -105,7 +105,7 @@ const AddDrawer = ({
                 id="customerDisplayName"
                 name="customerDisplayName"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="Display Name"
+                placeholder="Display"
                 value={formData.customerDisplayName}
                 onChange={handleInputChange}
                 required
