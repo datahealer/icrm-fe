450c43bb865c2ada1de5c696016e420220783125
Hardik Garg <hardik@inzint.com>
2024-12-03 11:06:00 +0530

    update drawer

--- src/views/admin/people/components/UpdateDrawer.jsx
+++ src/views/admin/people/components/UpdateDrawer.jsx
@@ -134,6 +134,27 @@ const UpdateDrawer = ({
             required
           />
         </div>
+
+
+        <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                <span className="text-lg text-red-500">*</span> Personal Email
+              </label>
+              <input
+                type="email"
+                id="personalEmail"
+                name="personalEmail"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder="Your email"
+                value={idData.personalEmail}
+                onChange={handleUpdateChange}
+                required
+              />
+            </div>
+
         {/* <div className="mb-6">
                     <label
                       htmlFor="password"
