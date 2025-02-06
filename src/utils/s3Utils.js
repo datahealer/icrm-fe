80156d3ae70858128a5fc51a1b33ff5acde3c831
Hardik Garg <hardik@inzint.com>
2025-02-06 10:16:22 +0530

    modify env var name

--- src/utils/s3Utils.js
+++ src/utils/s3Utils.js
@@ -1,13 +1,13 @@
 import AWS from "aws-sdk";
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: "ap-south-1",
 });
 
 export const uploadFileToS3 = async (file, folder) => {
   const params = {
-    Bucket: process.env.AWS_BUCKET,
+    Bucket: process.env.BUCKET_NAME,
     Key: `${folder}/${Date.now()}-${file.name}`,
     Body: file,
     ContentType: file.type,
@@ -16,7 +16,7 @@ export const uploadFileToS3 = async (file, folder) => {
   try {
     const { Key } = await s3.upload(params).promise();
     const signedUrl = s3.getSignedUrl("getObject", {
-      Bucket: process.env.AWS_BUCKET,
+      Bucket: process.env.BUCKET_NAME,
       Key,
       Expires: 60 * 60,
     });
