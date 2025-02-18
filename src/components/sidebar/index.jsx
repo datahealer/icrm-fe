0031b19c3bb92714d32cdb1e3bef7ead14c15008
Hardik Garg <hardik@inzint.com>
2025-02-18 20:27:40 +0530

    update logo url

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -21,7 +21,7 @@ const Sidebar = ({ open, onClose }) => {
 {/* Horizon <span class="font-medium">FREE</span> */}
       <div className={`mx-[56px] mt-[50px] flex items-center`}>
         <div className="mt-1 ml-1 h-2.5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
-          <img src="https://inzint.com/wp-content/uploads/2022/09/inzint-logo-dark.png" alt="" className="h-12 w-32" />
+          <img src="https://inzint-email-attachment.s3.ap-south-1.amazonaws.com/inzint_cover.jpeg" alt="" className="h-12 w-32" />
         </div>
       </div>
       <div class="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30" />
